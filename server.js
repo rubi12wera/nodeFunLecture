0f631734f0da870386ba91df87ff1b2a25901537
adriana <rubi12wera@gmail.com>
2015-08-10 13:24:25 -0600

    starting with twilio

--- server.js
+++ server.js
@@ -7,11 +7,50 @@ create a .gitignore file
 /////////////////////////////////
 var express = require('express');
 var bodyparser= require("body-parser");
-
+var cors = require('cors');
 var app = express();
+var request = require('request');
+var twilio = require('twilio')
 
 var port = 3000;
+/////////////////////////////////
+////////////MIDDLEWARE////
+/////////////////////////////////
 
+
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
+app.use(bodyparser.json());
+app.use(cors())
+
+/////////////////////////////////
+/////END-POINTS////
+/////////////////////////////////
+var message = {"message" : "Hellos WOrld"}
+app.get('/api/message', function(req, res){
+	return res.json(message)
+	//return res.send(JSON.stringify{"message" : "Hellos WOrld"})
+})
+app.post('/api/receive_message', function(req, res){
+	console.log(req.body);
+	res.send();
+})
+
+app.post('/api/send_text_message', function(req, res){
+	console.log(req.body);
+	res.send();
+})
+
+
+
+
+
+
+
+
+///////LISTENING PORT////////
 
 app.listen(port, function() {
 	// body...
